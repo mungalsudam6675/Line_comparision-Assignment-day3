5ebeb715a807c2a5ff1d8910f630e27c505f311e
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-01-25 16:24:51 +0530

    Adding Line comparision Uc2

---
+++ Uc8.java
@@ -0,0 +1,21 @@
+public class Uc8{
+
+	public static void main(String args[]){
+
+		String s1="psychological";
+
+		String s2="psychological";
+
+		String s3="phychological";
+
+		String s4="python";
+
+	System.out.println(s1.equals(s2));
+
+	System.out.println(s1.equals(s3));
+
+	System.out.println(s1.equals(s4));
+
+	}
+
+}
